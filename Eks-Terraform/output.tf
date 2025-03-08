955d33656539f3ac652a007b9e7dc456d01d460f
Manoj kumar Siriparthi <manojs.devops1@gmail.com>
2025-03-08 15:34:10 +0530

    Update output.tf

--- Eks-Terraform/output.tf
+++ Eks-Terraform/output.tf
@@ -1,16 +1,12 @@
 output "eks_cluster_endpoint" {
   description = "EKS Cluster Endpoint"
-  value       = aws_eks_cluster.gbfs_cluster.endpoint
+  value       = aws_eks_cluster.opensearch_cluster.endpoint
 }
 
 output "eks_cluster_certificate_authority" {
   description = "EKS Cluster Certificate Authority Data"
-  value       = aws_eks_cluster.gbfs_cluster.certificate_authority[0].data
+  value       = aws_eks_cluster.opensearch_cluster.certificate_authority[0].data
 }
 
-output "rds_endpoint" {
-  description = "RDS MySQL endpoint"
-  value       = aws_db_instance.mysql.endpoint
-}
 
 
