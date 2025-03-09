96d0e2664880e1ebda63b2e2b471eb64be281f6c
Manoj kumar Siriparthi <manojs.devops1@gmail.com>
2025-03-09 11:40:31 +0530

    Update Tools-Install.sh

--- Tools-Install.sh
+++ Tools-Install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # For Ubuntu 22.04
-# Intsalling Java
 sudo apt update -y
-sudo apt install openjdk-17-jre -y
-sudo apt install openjdk-17-jdk -y
-java --version
 
 # Installing Jenkins
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
@@ -19,7 +15,6 @@ sudo apt-get install jenkins -y
 #!/bin/bash
 sudo apt update
 sudo apt install docker.io -y
-sudo usermod -aG docker jenkins
 sudo usermod -aG docker ubuntu
 sudo systemctl restart docker
 sudo chmod 777 /var/run/docker.sock
@@ -46,32 +41,4 @@ wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/sha
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
 sudo apt install terraform -y
-
-# Intalling Helm
-#! /bin/bash
-sudo snap install helm --classic
-
-# MY SQL Install
-#!/bin/bash
-sudo apt install mysql-client -y
-
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-
-kubectl create namespace monitoring
-helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring
-
-# Installing Trivy
-#!/bin/bash
-sudo apt-get install wget apt-transport-https gnupg lsb-release -y
-wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
-echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
-sudo apt update
-sudo apt install trivy -y
-
-# prometheus & grafana
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-kubectl create namespace monitoring
-helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring
 
