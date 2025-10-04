cf17ac1502b64811f7ff87b2a4db868978efef89
Manoj kumar Siriparthi <manojs.devops1@gmail.com>
2025-10-04 07:00:17 +0000

    new repo

--- Eks-Terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "gbfs-backup.bucket.2024"  # Use the S3 bucket you created for the state file
-    key            = "terraform/state/gbfs-project/terraform.tfstate"
-    region         = "eu-west-2"
-    encrypt        = true                           # Enable encryption
-  }
-}
